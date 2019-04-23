f8d0f68c376c60a31d2fc45eb41e7e61b43208d9
Burçin Danacı <burcindanaci@hotmail.com>
2019-04-23 16:21:35 +0200

    Modified test_orthonormalization method in File 4a

--- mps/test/test_TEBD.py
+++ mps/test/test_TEBD.py
@@ -6,35 +6,42 @@ from mps.test.tools import *
 
 
 class TestTEBD_sweep(unittest.TestCase):
+    
     def test_orthonormalization(self):
+        #
+        # We verify that our two-site orthonormalization procedure, 
+        # does not change the state
+        #
         δt = 0.1
         tol = 0
-        H = mps.evolution.TINNHamiltonian(0*σx, σx, σx)
-        Trotter = mps.evolution.Trotter_unitaries(H, δt)
+        H = TINNHamiltonian(0*σx, σx, σx)
+        Trotter = Trotter_unitaries(H, δt)
 
         def left_ok(Ψ):
             for start in range(Ψ.size-1):            
-                AA = mps.evolution.apply_2siteTrotter(Trotter.twosite_unitary(start) , Ψ, start)
+                AA = apply_2siteTrotter(Trotter.twosite_unitary(start) , 
+                                                      Ψ, start)
                 A, AC = mps.state.left_orth_2site(AA,tol)
                 AA_orth = np.einsum("ijk,klm -> ijlm", A, AC)
                 self.assertTrue(similar(AA,AA_orth))
             
         test_over_random_mps(left_ok)
-
         
         def right_ok(Ψ):
-            δt = 0.1
-            ξ = Ψ.copy()
-            start = Ψ.size //2
-            H = mps.evolution.TINNHamiltonian(0*σx, σx, σx)
-            Trotter = mps.evolution.Trotter_unitaries(H, δt)
-            AA = mps.evolution.apply_2siteTrotter(Trotter.twosite_unitary(start) , ξ, start)
-            tol = 0
-            A, AC = mps.state.right_orth_2site(AA,tol)
-            AA_orth = np.einsum("ijk,klm -> ijlm", AC, A)
-            return np.isclose(AA,AA_orth)
+            for start in range(Ψ.size-1):            
+                AA = apply_2siteTrotter(Trotter.twosite_unitary(start) , 
+                                                      Ψ, start)
+                A, AC = mps.state.right_orth_2site(AA,tol)
+                AA_orth = np.einsum("ijk,klm -> ijlm", AC, A)
+                self.assertTrue(similar(AA,AA_orth))
             
         test_over_random_mps(right_ok)
+    
+
+        
+       
+        
+    
 
 
 
