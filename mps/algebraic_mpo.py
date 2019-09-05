c7324b6de3d277144120c2ae3a887bf0e6dd9842
Juanjo Garcia Ripoll <juanjose.garciaripoll@gmail.com>
2019-09-05 10:44:21 +0200

    Added twoscomplement() and nonlinear product of wavefunctions.

--- mps/algebraic_mpo.py
+++ mps/algebraic_mpo.py
@@ -1,6 +1,8 @@
+import numpy as np
 import mps.state
 import mps.mpo
 from mps.mpo import MPO
+from mps.state import MPS
 
 def qubo_mpo(J=None, h=None, **kwdargs):
     """Return the MPO associated to a QUBO operator
@@ -106,4 +108,72 @@ def qubo_exponential_mpo(J=None, h=None, **kwdargs):
             data[-1] = A[:,:,:,[0]] + A[:,:,:,[1]]
             out.append(MPO(data, **kwdargs))
         return MPOList(out)
+
+def wavefunction_product(ψ, ξ, conjugate=False, simplify=True, **kwdargs):
+    """Implement a nonlinear transformation that multiplies two MPS, to
+    create a new MPS with combined bond dimensions.
+    
+    Arguments
+    ---------
+    ψ, ξ      -- Two MPS or CanonicalMPS.
+    conjugate -- Conjugate ψ or not.
+    simplify  -- Simplify the state afterwards or not.
+    kwdargs   -- Arguments to simplify() if simplify is True.
+    
+    Output
+    ------
+    ψξ        -- An MPS product of both.
+    """
+    
+    def combine(A, B):
+        # Combine both tensors
+        a, d, b = A.shape
+        c, d, e = B.shape
+        if conjugate:
+            A = A.conj()
+        D = np.array([np.outer(A[:,i,:].flatten(), B[:,i,:].flatten()) for i in range(d)])
+        D = np.einsum('iabce->acibe', np.array(D).reshape(d,a,b,c,e)).reshape(a*c,d,b*e)
+        return D
+
+    out = MPS([combine(A,B) for A,B in zip(ψ,ξ)])
+    if simplify:
+        out = CanonicalMPS(out, center=0, **kwdargs)
+        out, _, _ = mps.truncate.simplify(out, **kwdargs)
+    return out
+
+def twoscomplement(L, control=0, sites=None, **kwdargs):
+    """Return an MPO that performs a two's complement of the selected qubits
+    depending on a 'control' qubit in a register with L qubits.
+    
+    Arguments
+    ---------
+    L       -- Real size of register
+    control -- Which qubit (relative to sites) controls the sign.
+               Defaults to the first qubit in 'sites'.
+    sites   -- The qubits involved in the MPO. Defaults to range(L).
+    kwdargs -- Arguments for MPO.
+    
+    Returns
+    -------
+    mpo     -- An MPO object
+    """
+    
+    if sites is not None:
+        out = twoscomplement(len(sites), control=control, sites=None, **kwdargs)
+        return out.extend(L, sites=sites)
+    else:
+        A0 = np.zeros((2,2,2,2))
+        A0[0,0,0,0] = 1.
+        A0[1,1,1,1] = 1.
+        A = np.zeros((2,2,2,2))
+        A[0,0,0,0] = 1.
+        A[0,1,1,0] = 1.
+        A[1,1,0,1] = 1.
+        A[1,0,1,1] = 1.
+        data = [A0 if i == control else A for i in range(L)]
+        A = data[0]
+        data[0] = A[[0],:,:,:] + A[[1],:,:,:]
+        A = data[-1]
+        data[-1] = A[:,:,:,[0]] + A[:,:,:,[1]]
+        return MPO(data, **kwdargs)
 from mps.algebraic_mpo import * 
