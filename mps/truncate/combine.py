bb17c5a5c4762b296c7d3f7360a759f808e8a610
Juan Jose Garcia Ripoll <juanjose.garciaripoll@gmail.com>
2022-07-24 16:49:16 +0200

    combine() and simplify() now log less information and stop if errors fail to decrease

--- mps/truncate/combine.py
+++ mps/truncate/combine.py
@@ -2,9 +2,11 @@ import numpy as np
 import math
 import mps
 import mps.state
+from mps.expectation import scprod
 from mps.state.truncation import DEFAULT_TOLERANCE
 from mps.tools import log
 from .simplify import AntilinearForm
+
 
 def multi_norm_squared(α, ψ):
     """Compute the norm-squared of the vector sum(α[i]*ψ[i])"""
@@ -14,6 +16,36 @@ def multi_norm_squared(α, ψ):
             c += 2 * (αi.conjugate() * α[j] * scprod(ψ[i], ψ[j])).real
         c += np.abs(αi) ** 2 * scprod(ψ[i], ψ[i]).real
     return c
+
+
+def guess_combine_state(weights, states):
+    guess = []
+    weighted_states = []
+    for i, state in enumerate(states):
+        weighted_states.append(weights[i] * state)
+    for site in range(states[0].size):
+        DL_max = 0
+        DR_max = 0
+        z = 0
+        for state in states:
+            z += state[site][0, 0, 0]
+            DL, i, DR = state[site].shape
+            if DL > DL_max:
+                DL_max = DL
+            if DR > DR_max:
+                DR_max = DR
+        guess.append(np.zeros((DL_max, i, DR_max), dtype=type(z)))
+    for site in range(states[0].size):
+        for j, state in enumerate(weighted_states):
+            DL, i, DR = state[site].shape
+            guess[site][:DL, :, :DR] += state[site]
+    return mps.state.MPS(
+        guess,
+        maxsweeps=states[0].maxsweeps,
+        tolerance=states[0].tolerance,
+        normalize=states[0].normalize,
+        max_bond_dimension=states[0].max_bond_dimension,
+    )
 
 
 def combine(
@@ -45,9 +77,8 @@ def combine(
     error     -- error made in the approximation
     """
     start = 0 if direction > 0 else size - 1
-
     if guess is None:
-        guess = states[0]
+        guess = guess_combine_state(weights, states)
     base_error = sum(
         np.sqrt(np.abs(α)) * np.sqrt(ψ.error()) for α, ψ in zip(weights, states)
     )
@@ -55,8 +86,9 @@ def combine(
     err = norm_ψsqr = multi_norm_squared(weights, states)
     if norm_ψsqr < tolerance:
         return mps.state.MPS([np.zeros((1, P.shape[1], 1)) for P in φ]), 0
-    log(f"Approximating ψ with |ψ|={norm_ψsqr**0.5} for {maxsweeps} sweeps.")
-    log(f"Weights: {weights}")
+    log(
+        f"COMBINE ψ with |ψ|={norm_ψsqr**0.5} for {maxsweeps} sweeps.\nWeights: {weights}"
+    )
 
     L = len(states)
     size = φ.size
@@ -64,9 +96,6 @@ def combine(
     for sweep in range(maxsweeps):
         if direction > 0:
             for n in range(0, size - 1):
-                log(
-                    f"Updating sites ({n},{n+1}) left-to-right, form.center={forms[0].center}, φ.center={φ.center}"
-                )
                 tensor = sum(
                     α * f.tensor2site(direction) for α, f in zip(weights, forms)
                 )
@@ -77,9 +106,6 @@ def combine(
                     f.update(direction)
         else:
             for n in reversed(range(0, size - 1)):
-                log(
-                    f"Updating sites ({n},{n+1}) right-to-left, form.center={forms[0].center}, φ.center={φ.center}"
-                )
                 tensor = sum(
                     α * f.tensor2site(direction) for α, f in zip(weights, forms)
                 )
@@ -102,8 +128,8 @@ def combine(
         scprod_φψ = np.vdot(B, C)
         old_err = err
         err = 2 * abs(1.0 - scprod_φψ.real / np.sqrt(norm_φsqr * norm_ψsqr))
-        log(f"rel.err.={err}, old err.={old_err}, |φ|={norm_φsqr**0.5}")
-        if err < tolerance:
+        log(f"sweep={sweep}, rel.err.={err}, old err.={old_err}, |φ|={norm_φsqr**0.5}")
+        if err < tolerance or err > old_err:
             log(f"Stopping, as tolerance reached")
             break
         direction = -direction
--- mps/truncate/simplify.py
+++ mps/truncate/simplify.py
@@ -134,7 +134,9 @@ def simplify(
     form = AntilinearForm(φ, ψ, center=start)
     norm_ψsqr = scprod(ψ, ψ).real
     err = 1.0
-    log(f"SIMPLIFY ψ with |ψ|={norm_ψsqr**0.5} for {maxsweeps} sweeps.")
+    log(
+        f"SIMPLIFY ψ with |ψ|={norm_ψsqr**0.5} for {maxsweeps} sweeps, with tolerance {tolerance}."
+    )
     for sweep in range(maxsweeps):
         if direction > 0:
             for n in range(0, size - 1):
@@ -173,7 +175,7 @@ def simplify(
         old_err = err
         err = 2 * abs(1.0 - scprod_φψ.real / np.sqrt(norm_φsqr * norm_ψsqr))
         log(f"sweep={sweep}, rel.err.={err}, old err.={old_err}, |φ|={norm_φsqr**0.5}")
-        if err < tolerance:
+        if err < tolerance or err > old_err:
             log("Stopping, as tolerance reached")
             break
         direction = -direction
